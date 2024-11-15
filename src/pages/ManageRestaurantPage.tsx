645cdea56712e32dcd10be5c38652df3a678d855
Shiro12222 <leo12190818@gmail.com>
2024-11-15 22:24:32 +0800

    Update RestaurantApi

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,7 +1,12 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import RestaurantForm from "@/forms/manageRestaurantForms/RestaurantForm";
 
 const ManageRestaurantPage = () => {
-    return <RestaurantForm/>;
-}
+    const { createRestaurant, isLoading } = useCreateMyRestaurant();
+
+    return (
+        <RestaurantForm onSave={createRestaurant} isLoading={isLoading} />
+    );
+};
 
 export default ManageRestaurantPage;
