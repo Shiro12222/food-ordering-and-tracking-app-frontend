577a812ea378b86931491f8b9b94077b91a9bd57
Shiro12222 <leo12190818@gmail.com>
2024-11-18 21:05:24 +0800

    Debugging Progress

--- src/api/RestaurantApi.tsx
+++ src/api/RestaurantApi.tsx
@@ -6,7 +6,8 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useSearchRestaurants = (city?: string) => {
     const createSearchRequest = async(): Promise<RestaurantSearchResponse> => {
-        const response = await fetch(`${API_BASE_URL}/api/restaurant/search${city}`
+        const response = await fetch(
+            `${API_BASE_URL}/api/restaurant/search/${city}`
         );
 
         if(!response.ok){
